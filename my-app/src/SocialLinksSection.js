8a8579a0b4759798fa3991a3edb5a96cac1195f2
Penny-21 <vpennacchio02@gmail.com>
2024-05-20 11:24:36 +0200

    Social links changed size and color

--- my-app/src/SocialLinksSection.js
+++ my-app/src/SocialLinksSection.js
@@ -1,8 +1,17 @@
 import React from "react";
 /*import './SocialLinksSection.css'; */// Importa il file CSS per la stilizzazione
-import {ReactComponent as Instagram} from "./svg-bank/square-instagram.svg"
-import {ReactComponent as Facebook} from "./svg-bank/square-facebook.svg"
-import {ReactComponent as Linkedin} from "./svg-bank/linkedin.svg"
+import {ReactComponent as Instagram} from "./svg-bank/square-instagram.svg";
+import {ReactComponent as Facebook} from "./svg-bank/square-facebook.svg";
+import {ReactComponent as Linkedin} from "./svg-bank/linkedin.svg";
+
+/* const colors = {
+  primary: '#405DE6',
+  secondary: '#5851DB',
+  tertiary: '#833AB4',
+  quaternary: '#C13584',
+  quinary: '#E1306C'
+}; */
+
 
 function SocialLinksSection() {
   return (
@@ -15,7 +24,7 @@ function SocialLinksSection() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Instagram />
+              <Instagram style={{ width: '50px', height:'50px', fill: '#C13584' }} />
             </a>
           </div>
           <div className="col-md-4">
@@ -24,7 +33,7 @@ function SocialLinksSection() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Facebook />
+              <Facebook style={{ width: '50px', height:'50px', fill: '#0165E1' }} />
             </a>
           </div>
           <div className="col-md-4">
@@ -33,7 +42,7 @@ function SocialLinksSection() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Linkedin />
+              <Linkedin style={{ width: '50px', height:'50px', fill: '#0A66C2' }} />
             </a>
           </div>
         </div>
