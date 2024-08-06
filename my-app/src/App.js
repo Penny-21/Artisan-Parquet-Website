18682357d92a35ae8483dd26ed8245eb56437660
Penny-21 <vpennacchio02@gmail.com>
2024-08-06 12:17:19 +0300

    Fixing for router function but still got some issues

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
 import "./App.css";
-import Navbar from './Navbar-folder/Navbar'; // Importa il componente Navbar
-import PreFooterSection from './Prefooter-folder/PreFooterSection';
-import Footer from './Footer-folder/Footer';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa Router e Route
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Navbar from "./Navbar-folder/Navbar"; // Importa il componente Navbar
+import Home from "./Home-folder/Home"; // Assicurati che il percorso sia corretto
+import About from "./About-folder/About"; // Assicurati che il percorso sia corretto
+import Collections from "./Collections-folder/Collections"; // Assicurati che il percorso sia corretto
+import Portfolio from "./Portfolio-folder/Portfolio"; // Assicurati che il percorso sia corretto
+import Stories from "./Stories-folder/Stories"; // Assicurati che il percorso sia corretto
+import Contact from "./Contact-folder/Contact"; // Assicurati che il percorso sia corretto
+import Carousel from "./Carousel-folder/Carousel";
+import TextSection from "./Text-section-folder/TextSection";
+import ImageSection from "./Imagesection-folder/ImageSection";
+import ReviewSection from "./Review-section-folder/ReviewSection";
+import SocialLinksSection from "./Social-links-folder/SocialLinksSection";
+import PreFooterSection from "./Prefooter-folder/PreFooterSection";
+import Footer from "./Footer-folder/Footer";
+
 
 function App() {
   return (
@@ -21,7 +38,7 @@ function App() {
         </a>
       </header>
     </div> */
-    <Router>
+    /*     <Router>
       <div className="App">
         <Navbar />
         <Switch>
@@ -31,7 +48,20 @@ function App() {
         <PreFooterSection />
         <Footer />
       </div>
-    </Router>
+    </Router> */
+    <React.StrictMode>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/stories" element={<Stories />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <PreFooterSection />
+      <Footer />
+    </React.StrictMode>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,38 +1,15 @@
 import React from "react";
+import App from "./App";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa Router e Route
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Navbar from "./Navbar-folder/Navbar"; // Importa il componente Navbar
-import Home from "./Home-folder/Home"; // Assicurati che il percorso sia corretto
-import About from "./About-folder/About"; // Assicurati che il percorso sia corretto
-import Collections from "./Collections-folder/Collections"; // Assicurati che il percorso sia corretto
-import Portfolio from "./Portfolio-folder/Portfolio"; // Assicurati che il percorso sia corretto
-import Stories from "./Stories-folder/Stories"; // Assicurati che il percorso sia corretto
-import Contact from "./Contact-folder/Contact"; // Assicurati che il percorso sia corretto
-import Carousel from "./Carousel-folder/Carousel";
-import TextSection from "./Text-section-folder/TextSection";
-import ImageSection from "./Imagesection-folder/ImageSection";
-import ReviewSection from "./Review-section-folder/ReviewSection";
-import SocialLinksSection from "./Social-links-folder/SocialLinksSection";
-import PreFooterSection from "./Prefooter-folder/PreFooterSection";
-import Footer from "./Footer-folder/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navbar />
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/About-folder" component={About} />
-        <Route path="/Collections-folder" component={Collections} />
-        <Route path="/Portfolio-folder" component={Portfolio} />
-        <Route path="/Stories-folder" component={Stories} />
-        <Route path="/Contact-folder" component={Contact} />
-      </Switch>
-      <PreFooterSection />
-      <Footer />
+      <App />  {/* App contiene Navbar, Routes, PreFooter, Footer */}
     </Router>
   </React.StrictMode>
 );
